182f49eb57fe552e9a653682b12b13ebd68e7168
cwaring <chris@wwaves.co>
2021-06-28 12:20:53 +0100

    default to no trailing slash

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 type Base = string | undefined
 
-// construct current base path inc ipfs/ipns variants
+// construct current base path for ipfs/ipns variants
 const getBase = (): Base => {
   if (typeof window !== 'undefined') {
     const ipfsRx = /^(\/(?:ipfs|ipns)\/[^/]+)/
     const pathname = window.location.pathname
-    const ipfsPrefix = (pathname.match(ipfsRx) || [])[1] || ''
-    return `${ipfsPrefix}/`
+    return (pathname.match(ipfsRx) || [])[1] || undefined
   }
 }
 
-// create <base /> element and return the current root base path
+// create <base /> element and return the current root base path if an IPFS gateway is detected
 const createBase = (): Base => {
   const base = getBase()
   if (base) {
     const baseEl = document.createElement('base')
-    baseEl.href = base
+    baseEl.href = base + '/'
     document.getElementsByTagName('head')[0].appendChild(baseEl)
     return base
   }
