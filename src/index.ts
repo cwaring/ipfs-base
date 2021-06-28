8e58ed4b06ed3db715359279e61e7a0f1527fb18
cwaring <chris@wwaves.co>
2021-06-28 13:57:15 +0100

    add basePath option

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 type Base = string | undefined
+type basePath = string | undefined
+
+const checkBase = (basePath?: basePath) => {
+  if (basePath) {
+    const baseRx = /^\/\w.*[^/]$/
+    if (!basePath.match(baseRx)) {
+      console.error(`Error[ipfs-base]: basePath=${basePath}, a correctly formatted basePath should start with / and have no trailing slash, eg: /path`)
+      return false
+    }
+  }
+  return true
+}
 
 // construct current base path for ipfs/ipns variants
-const getBase = (): Base => {
-  if (typeof window !== 'undefined') {
+const getBase = (basePath?: basePath): Base => {
+  if (typeof window !== 'undefined' && checkBase(basePath)) {
     const ipfsRx = /^(\/(?:ipfs|ipns)\/[^/]+)/
     const pathname = window.location.pathname
-    return (pathname.match(ipfsRx) || [])[1] || undefined
+    return basePath ? ((pathname.match(ipfsRx) || [])[1] || '') + (basePath || '') : basePath
   }
 }
 
 // create <base /> element and return the current root base path if an IPFS gateway is detected
-const createBase = (): Base => {
-  const base = getBase()
+const createBase = (basePath?: basePath): Base => {
+  const base = getBase(basePath)
   if (base) {
     const baseEl = document.createElement('base')
     baseEl.href = base + '/'
